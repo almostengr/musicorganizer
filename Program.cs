31e76240f6dd5f686be7fe4ec9fc159dc2d86a74
Kenny Robinson <bitsecondal@gmail.com>
2022-06-10 19:23:26 -0500

    Dev (#5)
    * Updated dto to record
    
    * Updated project properties for release
    
    * Closed #2; #1 added recursive directory search; updated logging; cleaned code; added resource to readme
    
    * Closed #1 Closed #3; Updated tasks, project for releases
    
    * Update README.md
    
    * Renamed extensions class for DTO
    
    * Moved program logic to service methods; accept CLI args for directories
    
    * added permission to service methods
    
    * Added help for command line usage
    
    * Updated command line args display; update task running

--- Program.cs
+++ Program.cs
@@ -1,108 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
-using Almostengr.MusicOrganizer.DataTransferObjects;
-using Almostengr.MusicOrganizer.Extensions;
-using Newtonsoft.Json;
+using Almostengr.MusicOrganizer.Services;
 
-const string OLD_MUSIC_DIRECTORY = "/home/almostengineer/Downloads/oldMusic";
-const string NEW_MUSIC_DIRECTORY = "/home/almostengineer/Downloads/newMusic";
-
-if (Directory.Exists(NEW_MUSIC_DIRECTORY) == false)
+if (args.Length != 4)
 {
-    Directory.CreateDirectory(NEW_MUSIC_DIRECTORY);
-}
-
-if (Directory.Exists(OLD_MUSIC_DIRECTORY) == false)
-{
-    Console.WriteLine("Old music directory does not exist");
+    Console.WriteLine("Not enough arguments passed");
+    Console.WriteLine();
+    Console.WriteLine("Usage");
+    Console.WriteLine("Almostengr.MusicOrganizer.dll -old <original directory> -new <new directory>");
     return;
 }
 
-ProcessMusicFiles(OLD_MUSIC_DIRECTORY);
+string oldDirectory = string.Empty;
+string newDirectory = string.Empty;
 
-
-static void ProcessMusicFiles(string directoryName)
+for (int i = 0; i < args.Length; i++)
 {
-    Console.WriteLine($"Checking contents of {directoryName}");
-
-    if (Directory.Exists(directoryName) == false)
+    switch (args[i])
     {
-        return;
+        case "-old":
+            oldDirectory = args[i + 1];
+            break;
+
+        case "-new":
+            newDirectory = args[i + 1];
+            break;
+
+        default:
+            break;
     }
+}
 
-    foreach (var directory in Directory.GetDirectories(directoryName))
-    {
-        ProcessMusicFiles(directory);
-    }
-
-    UpdateAndMoveMusicFiles(directoryName);
-} // end method
-
-static void UpdateAndMoveMusicFiles(string directory)
+if (string.IsNullOrEmpty(oldDirectory) || Directory.Exists(oldDirectory) == false)
 {
-    foreach (string musicFile in Directory.GetFiles(directory, "*mp3"))
-    {
-        Console.WriteLine($"Processing {musicFile}");
 
-        // ffprobe -v quiet -print_format json -show_format -show_streams "music file.mp3"
-        Process ffprobe = new Process
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = "/bin/bash",
-                Arguments = $"-c \"/usr/bin/ffprobe -v quiet -print_format json -show_format \\\"{musicFile}\\\"\"",
-                WorkingDirectory = OLD_MUSIC_DIRECTORY,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            }
-        };
+    Console.WriteLine("Old music directory does not exist or is not valid.");
+    return;
+}
 
-        ffprobe.Start();
-        ffprobe.WaitForExit();
+if (string.IsNullOrEmpty(newDirectory))
+{
+    Console.WriteLine("New music directory is not valid.");
+    return;
+}
 
-        string output = ffprobe.StandardOutput.ReadToEnd();
 
-        ffprobe.Close();
-        ffprobe.Dispose();
-
-        FfprobeMetaDTO metaData = JsonConvert.DeserializeObject<FfprobeMetaDTO>(output);
-
-        string artistDirectory = Path.Combine(NEW_MUSIC_DIRECTORY, metaData.ToArtistOrAlbumArtist());
-
-        if (Directory.Exists(artistDirectory) == false)
-        {
-            Directory.CreateDirectory(artistDirectory);
-        }
-
-        string albumDirectory =
-            Path.Combine(artistDirectory, metaData.Format.Tags.Album.ToAlbumNameOrDefault());
-
-        if (Directory.Exists(albumDirectory) == false)
-        {
-            Directory.CreateDirectory(albumDirectory);
-        }
-
-        string newMusicFileName = Path.Combine(
-            albumDirectory,
-            $"{metaData.Format.Tags.Track.ToTrackNumberOrDefault()}_{metaData.Format.Tags.Title.ToTrackTitleOrDefault()}_{metaData.ToArtistOrAlbumArtist()}.mp3");
-
-        if (File.Exists(newMusicFileName))
-        {
-            Console.WriteLine($"{newMusicFileName} already exists. {musicFile} was not moved and considered a duplicate.");
-            continue;
-        }
-
-        try
-        {
-            File.Move(musicFile, newMusicFileName);
-            Console.WriteLine($"Moved {musicFile} to {newMusicFileName})");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Unable to move file. {ex.Message}");
-        }
-
-    }
-} // end method
+IMusicOrganizerService musicOrganizerService = new MusicOrganizerService(oldDirectory, newDirectory);
+musicOrganizerService.CleanMusicCollection();
